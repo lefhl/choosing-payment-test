0355fadc37a92e8aea1404ecbe89833f5c43d323
eduard <gizbreht.e@yandex.ru>
2024-07-18 16:17:15 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/choosing-payment-test/',
+  // base: '/choosing-payment-test/',
   plugins: [vue()],
   resolve: {
     alias: {
